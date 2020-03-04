b88ebac05fef5098e2e557a7d3504fbb60b656c6
Güttinger Luca <luca.guettinger@cmiag.ch>
2020-03-04 16:01:04 +0100

    try adding set methods

--- src/main/java/net/myplayplanet/jpatest/controller/UserController.java
+++ src/main/java/net/myplayplanet/jpatest/controller/UserController.java
@@ -4,22 +4,35 @@ import net.myplayplanet.jpatest.dtos.UserDto;
 import net.myplayplanet.jpatest.model.UserTestEntity;
 import net.myplayplanet.jpatest.repository.UserTestEntityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 @RestController
 public class UserController {
-    @Autowired
-    private UserTestEntityRepository userTestRepository;
+    @PersistenceContext
+    EntityManager entityManager;
+    private UserTestEntityRepository userTestEntityRepository;
 
+    @Autowired
+    public UserController(UserTestEntityRepository userTestEntityRepository) {
+        this.userTestEntityRepository = userTestEntityRepository;
+    }
 
     @GetMapping("user/get/{id}")
     public UserDto getUser(@PathVariable String id) {
-        final UserTestEntity one = this.userTestRepository.getOne(id);
-
+        final UserTestEntity one = this.userTestEntityRepository.getOne(id);
+        System.out.println(one);
         final UserDto userDto = new UserDto();
         userDto.setUsername(one.getUsername());
         return userDto;
+    }
+    @Transactional
+    @PostMapping("user/set/")
+    public void setUser(@RequestBody UserTestEntity user) {
+        System.out.println(user.getUsername());
+        this.entityManager.persist(user);
     }
 }
--- src/main/java/net/myplayplanet/jpatest/model/UserTestEntity.java
+++ src/main/java/net/myplayplanet/jpatest/model/UserTestEntity.java
@@ -20,6 +20,4 @@ public class UserTestEntity implements Serializable {
 
     @Column(name = "age", nullable = false)
     private Integer age;
-
-    
 }
