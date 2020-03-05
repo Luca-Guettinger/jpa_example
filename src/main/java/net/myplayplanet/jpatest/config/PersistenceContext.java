1fa939836c62a7183cb9bf781189a0cee34de574
LucaGuettinger <luca.guettinger@cmiag.ch>
2020-03-05 08:33:17 +0100

    make it work with mysql

--- src/main/java/net/myplayplanet/jpatest/config/PersistenceContext.java
+++ src/main/java/net/myplayplanet/jpatest/config/PersistenceContext.java
@@ -1,20 +1,9 @@
 package net.myplayplanet.jpatest.config;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.annotation.Transactional;
-
-import javax.persistence.EntityManagerFactory;
-import javax.sql.DataSource;
-import java.util.Properties;
 
 @Configuration
 @EnableTransactionManagement
@@ -23,15 +12,5 @@ import java.util.Properties;
 })
 @Transactional
 public class PersistenceContext {
-    @Bean(destroyMethod = "close")
-    DataSource dataSource(Environment env) {
-        HikariConfig dataSourceConfig = new HikariConfig();
-        dataSourceConfig.setDriverClassName(env.getRequiredProperty("db.driver"));
-        dataSourceConfig.setJdbcUrl(env.getRequiredProperty("db.url"));
-        dataSourceConfig.setUsername(env.getRequiredProperty("db.username"));
-        dataSourceConfig.setPassword(env.getRequiredProperty("db.password"));
-
-        return new HikariDataSource(dataSourceConfig);
-    }
 
 }
